f1be0e58ae511d1e0eb9f73e1b5a7392517d3ecd
aaron.lee <aaron.lee@vivotek.com>
2015-04-29 10:12:18 +0800

    [A] Add pthread create with detached state

---
+++ c_cc/pthread_create_detached/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void *worker(void *data)
+{
+    printf("QQ\n");
+}
+
+int main()
+{
+    pthread_t tid = 0;
+    pthread_attr_t attr = {0};
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+
+    if (pthread_create(&tid, &attr, worker, NULL))
+    {
+        printf("Failed to create thread\n");
+        return 0;
+    }
+
+    pthread_detach(tid);
+
+    sleep(1);
+
+    return 0;
+}
