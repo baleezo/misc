21da597e372da2db54c0eb8a7406e1118034478f
aaron.lee <aaron.lee@vivotek.com>
2015-04-30 19:19:33 +0800

    [A] Add python c binding call cpp

---
+++ c_cc/python_c_binding_call_cpp/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <Python.h>
+#include "cppm.h"
+
+
+static PyObject *Call_testc(PyObject *self, PyObject *args)
+{
+    cpp_method();
+    Py_RETURN_TRUE;
+}
+
+static struct PyMethodDef testc_method[] = 
+{
+    {"testc", Call_testc, METH_VARARGS, NULL},
+    {NULL, NULL, 0, NULL}
+};
+
+void inittestc(void)
+{
+    Py_InitModule("testc", testc_method);
+}
+
