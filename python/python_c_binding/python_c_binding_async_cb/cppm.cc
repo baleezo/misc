1ccd58959a85aab08ea98f3eeeb0cc7f0d941fde
aaron.lee <aaron.lee@vivotek.com>
2015-05-05 11:28:00 +0800

    [M][python c binding async cb] Add cb data

--- python/python_c_binding/python_c_binding_async_cb/cppm.cc
+++ python/python_c_binding/python_c_binding_async_cb/cppm.cc
@@ -1,16 +1,23 @@
 #include <iostream>
-#include <pthread.h>
 #include <glib.h>
+#include <glib-object.h>
+#include <pthread.h>
 #include "cppm.h"
 
 using namespace std;
 
+typedef struct {
+    PyObject *cb;
+    gpointer data;
+} cb_info_t;
+
 // Wrapper for g_main_loop
 gboolean cb_wrapper(gpointer userdata)
 {
-    PyObject *cb = (PyObject *)userdata;
+    cb_info_t *cb_info = (cb_info_t *)userdata;
+    PyObject *cb = cb_info->cb;
     
-    PyObject *arglist = Py_BuildValue("(s)", "exec overed");
+    PyObject *arglist = Py_BuildValue("(s)", (char *)cb_info->data);
     cout << "Ready to callback" << endl;
     if (!PyCallable_Check(cb))
     {
@@ -30,13 +37,23 @@ gboolean cb_wrapper(gpointer userdata)
     return FALSE;
 }
 
+void free_cb_info(gpointer data)
+{
+    cb_info_t *cb_info = (cb_info_t *)data;
+    g_free(cb_info->data);
+    g_free(data);
+}
+
 void *worker(void *data)
 {
     // Wrapper for g_main_loop
-    g_timeout_add(0, cb_wrapper, (gpointer)data);
+    cb_info_t *cb_info = g_new0(cb_info_t, 1);
+    cb_info->cb = (PyObject *)data;
+    cb_info->data = g_strdup("async job exec overed");
+    g_timeout_add_full(G_PRIORITY_HIGH, 0, cb_wrapper, (gpointer)data, free_cb_info);
     return NULL;
 
-    /*
+    /* callback directly
     PyObject *cb = (PyObject *)data;
     PyObject *arglist = Py_BuildValue("(s)", "exec overed");
     cout << "Ready to callback" << endl;
