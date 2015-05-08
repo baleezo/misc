56cc86200d4c729f5992fbfa507ce9d7c60777b8
aaron.lee <aaron.lee@vivotek.com>
2015-05-08 16:24:06 +0800

    [M][python cpp obj wrapper by ctypes] print more info of ret objs

--- python/python_c_binding/python_cpp_obj_wrapper_by_ctypes/test.py
+++ python/python_c_binding/python_cpp_obj_wrapper_by_ctypes/test.py
@@ -4,10 +4,14 @@ from foo_wrapper import Foo
 def run():
     foo = Foo()
 
-    print foo.method_a()
-    print foo.method_b()
-    print foo.method_c()
-    print foo.method_d()
+    a = foo.method_a()
+    print type(a), a
+    b = foo.method_b()
+    print type(b), b
+    c = foo.method_c()
+    print type(c), c
+    d = foo.method_d()
+    print type(d), d
 
 
 if __name__ == '__main__':
