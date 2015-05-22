096c0b2397cda5e46185586053b56b7aaa90d037
aaron.lee <aaron.lee@vivotek.com>
2015-05-22 15:00:44 +0800

    [M][python cpp obj wrapper by ctypes] Use delete to release cpp obj

--- python/bindings/python_cpp_obj_wrapper_by_ctypes/foo.cc
+++ python/bindings/python_cpp_obj_wrapper_by_ctypes/foo.cc
@@ -46,7 +46,7 @@ PyObject *foo::method_d()
 
 extern "C" {
     foo *new_foo(){return new foo();}
-    void release_foo(foo *f){f->~foo();}
+    void release_foo(foo *f){delete f;}
     PyObject *foo_method_a(foo *f){return f->method_a();}
     PyObject *foo_method_b(foo *f, PyObject *obj){return f->method_b(obj);}
     PyObject *foo_method_c(foo *f, const char *in_str){return f->method_c(in_str);}
