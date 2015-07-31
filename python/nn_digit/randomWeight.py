8d3185d7c0fc1aa2eaf6852774814ebeca039359
aaron.lee <aaron.lee@vivotek.com>
2015-07-31 14:44:15 +0800

    [M] Move nn digit to machine learning dir

--- python/nn_digit/randomWeight.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-def random_init_weight(l_in, l_out):
-  epsilon_init = 0.12
-  return np.random.rand(l_out, l_in + 1) * 2 * epsilon_init - epsilon_init
--- python/nn_digit/utils.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-def merge_matrix_to_1d_array(*args):
-  return np.append(*(np.hstack(arg) for arg in args))
