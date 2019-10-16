bdd338c407c24466dda8bbebdaa7cf74b392a73b
baleezo <baleezo@gmail.com>
2019-10-16 21:12:37 +0800

    refactor

--- leetcode/3/opt.py
+++ leetcode/3/opt.py
@@ -1,14 +1,14 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         max_len = 0
-        start = 0
+        w_start = 0
         position_of_c = {}
 
-        for idx, c in enumerate(s):
+        for w_end, c in enumerate(s):
             if c in position_of_c:
-                start = max(start, position_of_c[c] + 1)
+                w_start = max(w_start, position_of_c[c] + 1)
 
-            max_len = max(max_len, idx - start + 1)
-            position_of_c[c] = idx
+            max_len = max(max_len, w_end - w_start + 1)
+            position_of_c[c] = w_end
                 
         return max_len
